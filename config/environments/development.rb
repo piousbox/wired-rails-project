63a31da71dcbb5b7752339d9ddb72fcf1c22f87a
Victor Piousbox <piousbox@gmail.com>
2015-10-17 12:56:28 -0700

    db config changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,8 @@
+
 Rails.application.configure do
+
+  config.active_record.table_name_prefix = 'tbx_'
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
